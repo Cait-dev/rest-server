283e4f3432619d41c7ac9b951f55517c7eec87cb
Raul sepulveda <Raulsepulveda12@hotmail.com>
2022-10-07 16:30:04 -0300

    Usuarios Crud listo - 07/10/2022

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,36 +1,59 @@
 const { response, request} = require('express');
+const bcryptjs = require('bcrypt');
 
 
-const usuariosGet = (req = request, res = response) =>  {
+const Usuario = require('../models/usuario')
 
-    const { q, nombre = 'No name', apikey, page = 1, limit} = req.query;
+const usuariosGet = async(req = request, res = response) =>  {
+
+    const {limite = 5, desde = 0} = req.query;
+    const query = {estado: true}
+
+
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite)),
+    ])
     res.json({
-        msg: 'get API - controlador',
-        q,
-        nombre,
-        apikey,
-        page,
-        limit,
+        total,
+        usuarios
     });
 }
 
-const usuariosPost = (req, res=response) =>  {
+const usuariosPost = async (req, res=response) =>  {
 
-    const {nombre, edad} = req.body;
+    const { nombre, correo, password, rol } = req.body;
+    const usuario = new Usuario( {nombre, correo, password, rol} );
+
+
+    //Hash de password
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt)
+
+    //Guardar en bd
+    await usuario.save();
+
     res.json({
-        msg: 'post API - controlador',
-        nombre,
-        edad,
+        usuario
     });
 }
 
-const usuariosPut = (req, res=response) =>  {
+const usuariosPut = async (req, res=response) =>  {
 
     const { id } = req.params;
-    res.json({
-        msg: 'put API - controlador',
-        id
-    });
+    const { _id ,password, google, correo, ...resto} = req.body;
+
+    //Validar contra base de datos
+    if (password){
+        //Hash de password
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync(password, salt)
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate(id,resto)
+    res.json(usuario);
 }
 
 const usuariosPatch = (req, res=response) =>  {
@@ -39,10 +62,15 @@ const usuariosPatch = (req, res=response) =>  {
     });
 }
 
-const usuariosDelete = (req, res=response) =>  {
-    res.json({
-        msg: 'delete API - controlador'
-    });
+const usuariosDelete = async(req, res=response) =>  {
+    const {id} = req.params;
+
+    //Borrar fisicamente usuario
+    //const usuario = await Usuario.findByIdAndDelete(id)
+
+    //Desactivar el usuario para no perder su informacion
+    const usuario = await Usuario.findByIdAndUpdate(id, {estado: false})
+    res.json(usuario);
   }
 
 module.exports = {
--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 
 class Server{
@@ -9,6 +10,10 @@ class Server{
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
 
+
+
+        //Conectar a base de datos
+        this.conectarDB();
         // Middlewares
         this.middlewares();
 
@@ -16,6 +21,10 @@ class Server{
         //rutas de mi app
         this.routes();
 
+    }
+
+    async conectarDB(){
+        await dbConnection();
     }
 
     middlewares(){
