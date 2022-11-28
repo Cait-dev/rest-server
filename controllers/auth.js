a4b4b7c98cab017b2496a4d461b30782b170c8a3
Raul sepulveda <Raulsepulveda12@hotmail.com>
2022-11-28 07:15:01 -0300

    google signIn y signOut

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,11 @@
-const {response} = require('express');
+const {response, json} = require('express');
 const bcryptjs = require('bcrypt');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
+const Usuario = require('../models/usuario')
 
 
 const login = async(req, res = response) => {
-const Usuario = require('../models/usuario')
     const {correo, password} = req.body;
 
     try {
@@ -43,6 +44,54 @@ const Usuario = require('../models/usuario')
 
 }
 
+const googleSingIn = async(req, res = response) => {
+
+    const {id_token} = req.body
+
+
+    try {
+
+        const {nombre, img, correo} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario){
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                rol: 'USER_ROLE',
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //Si el usuario en db
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el administrador - Usuario bloqueado'
+            })
+        }
+        //Generar JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        })
+        
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se puede verificar'
+        })
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
